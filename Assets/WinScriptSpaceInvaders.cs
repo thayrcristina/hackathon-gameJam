9f11137177093985ed9b845bf6dee1a904a09d36
thayrcristina <39676312+thayrcristina@users.noreply.github.com>
2018-10-02 11:32:30 -0300

    Add files via upload

---
+++ Assets/WinScriptSpaceInvaders.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScriptSpaceInvaders : MonoBehaviour {
+	
+	Text win;
+
+	// Use this for initialization
+	void Start () {
+		win = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GameOverScriptSpaceInvaders.morreu && ScoreScript.scoreValue > 20){
+			win.text = "WIN";
+			if (Input.GetKey ("r"))
+				Application.LoadLevel(Application.loadedLevel);
+			if (Input.GetKey (KeyCode.Space))
+				Application.LoadLevel("BomberMan");
+		}
+	}
+}
